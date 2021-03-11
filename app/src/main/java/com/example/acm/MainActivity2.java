278a60e4f397ae0c120e0e9ae5a4c6102ddbd55e
Devansh <mittaldevansh123@gmail.com>
2021-03-11 21:15:57 +0530

    Gallery UI Added

--- app/src/main/java/com/example/acm/MainActivity2.java
+++ app/src/main/java/com/example/acm/MainActivity2.java
@@ -9,7 +9,7 @@ import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity  implements View.OnClickListener{
 
-    public CardView card1,card2,card3,card4;
+    public CardView card1,card2,card3,card4,card5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +20,12 @@ public class MainActivity2 extends AppCompatActivity  implements View.OnClickLis
          card2=(CardView) findViewById(R.id.card_2);
         card3=(CardView) findViewById(R.id.card_3);
         card4=(CardView) findViewById(R.id.card_4);
-        card4.setOnClickListener(this);
+        card5=(CardView)findViewById(R.id.card_5);
         card1.setOnClickListener(this);
         card2.setOnClickListener(this);
         card3.setOnClickListener(this);
+        card4.setOnClickListener(this);
+        card5.setOnClickListener(this);
     }
 
     @Override
