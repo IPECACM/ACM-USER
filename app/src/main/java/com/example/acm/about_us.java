5571ad19cdbb4cd0a80f65b21bcc38f5e4776967
Devansh <mittaldevansh123@gmail.com>
2021-03-31 15:11:41 +0530

    insta and fb button added

--- app/src/main/java/com/example/acm/about_us.java
+++ app/src/main/java/com/example/acm/about_us.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
 
 public class about_us extends AppCompatActivity {
+
+    ImageView insta;
+    ImageView fb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,26 @@ public class about_us extends AppCompatActivity {
                 openmap();
             }
         });
+
+        insta=findViewById(R.id.insta);
+        fb=findViewById(R.id.fb);
+        insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotourl("https://instagram.com/ipec_acm_chapter?igshid=5r007wn3i8rd");
+            }
+        });
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotourl("http://www.facebook.com/IpecACM");
+            }
+        });
+    }
+
+    private void gotourl(String s) {
+        Uri uri =Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 
     private void openmap()
