6f2b39ef2162120595a15850a8889aefec0d06f0
Devansh <mittaldevansh123@gmail.com>
2021-04-24 18:08:42 +0530

    SIG SELECTION ACTIVITY DONE

---
+++ app/src/main/java/com/example/acm/sig_selection.java
@@ -0,0 +1,14 @@
+package com.example.acm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sig_selection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sig_selection);
+    }
+}
