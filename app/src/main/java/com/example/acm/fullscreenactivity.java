bd79d2188c742b1769fbf14f2c13374427ef0ff1
Devansh <mittaldevansh123@gmail.com>
2021-05-18 23:21:53 +0530

    gallery layout done

--- app/src/main/java/com/example/acm/fullscreenactivity.java
+++ app/src/main/java/com/example/acm/fullscreenactivity.java
@@ -2,11 +2,8 @@ package com.example.acm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
+import android.animation.Animator;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -15,17 +12,18 @@ import static java.sql.Types.NULL;
 
 public class fullscreenactivity extends AppCompatActivity {
 
+    private Animator currentAnimator;
+    private int shortAnimationDuration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreenactivity);
 
 
-        ImageView fullscreen=(ImageView) findViewById(R.id.fullscreen);
+        ImageView fullscreen = (ImageView) findViewById(R.id.fullscreen);
 //        fullscreen.setImageResource(getIntent().getIntExtra("Image",R.drawable.acm));
-        String image= getIntent().getStringExtra("Image");
+        String image = getIntent().getStringExtra("Image");
         Glide.with(getApplicationContext()).load(image).into(fullscreen);
-
-
     }
 }
