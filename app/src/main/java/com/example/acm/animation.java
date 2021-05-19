8321c8012e57e1af06c7d23313ac8f6e46715af4
Devansh <mittaldevansh123@gmail.com>
2021-05-19 23:56:47 +0530

    login and splash screen done

---
+++ app/src/main/java/com/example/acm/animation.java
@@ -0,0 +1,33 @@
+package com.example.acm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class animation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setTheme(R.style.splash_screen);
+        setContentView(R.layout.animation);
+        final Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+
+            @Override
+
+            public void run() {
+
+                Intent intent = new Intent(animation.this , MainActivity.class);
+
+                startActivity(intent);
+
+            }
+
+        }, 3000);
+    }
+}
